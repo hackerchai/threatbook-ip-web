cd6649fcef1279d35e568122771f5d5f8ad77d71
Dalka0064 <dalka0064@outlook.com>
2022-07-20 22:32:32 +0800

    添加了前端vue代码

---
+++ static/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <img src="./assets/logo.png" class="logo">
+      <h2>威胁情报管理系统</h2>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+:root {
+  --color: #fff;
+  --color-heighter: #05f2f2;
+  --color-bg-main: #040c1f;
+  --color-bg-api: #040c1f;
+  --color-bg-hover: #0d2738;
+  --color-primary: #158876;
+  --color-secondary: #0e2233;
+  --bg-secondary: #fff;
+  --border-color: #ddd;
+  --color-query: #05f2f2;
+  --color-hover-query: #009688;
+  --color-search-history-border: #05f2f2;
+  --color-websit: #a7bcd1;
+  --color-bg-websit: #041527;
+  --color-bg-websit-heighter: #040c1f;
+  --color-website-text: #05f2f2;
+  --color-autocomplate-border: #05f2f2;
+  --color-gray: #a7bcd1;
+  --color-autocomplate-bg: #040c1f;
+  --color-scrollbar-thumb: #119098;
+  --collapse-bg: #061e2e;
+  --collapse-bg-hover: #055360;
+  --collapse-color: #05f2f2;
+  --collaps-wrap-bg: #061e2e;
+  --collaps-wrap-color: #fff;
+  --dropdown-menu-bg: hsla(0, 0%, 100%, 0.06);
+  --dropdown-menu-color: #fff;
+  --poper-arrow-color: #061e2e;
+  --user-bg: rgba(5, 242, 242, 0.02);
+  --user-info-name-color: #fff;
+  --user-nav-color: #a7bcd1;
+  --person-center-bg: rgba(5, 242, 242, 0.02);
+  --input-border-color: #05f2f2;
+  --table-cell-hover: #0c2638;
+  --table-cell-border: #0c2638;
+  --table-cell-color: #fff;
+  --dropdown-bg: #041024;
+  --dropdown-hover-bg: #0d2738;
+  --dropdown-border: #05f2f2;
+  --dropdown-color: #fff;
+  --dropdown-checked-color: #05f2f2;
+  --button-confirm-color: #041527;
+  --button-cancel-color: #fff;
+  --button-cancel-bg: #061e2e;
+  --person-line: #061e2e;
+  --person-color: #606266;
+  --person-label-color: #a7bcd1;
+  --serarch-history-color: #a7bcd1;
+}
+
+#app {
+  text-align: center;
+  background-color: var(--color-bg-main);
+  min-height: 100vh;
+}
+
+#nav {
+  background-color: var(--brand-color);
+  height: 64px;
+  color: var(--color-heighter);
+  display: flex;
+  align-items: center;
+}
+.logo{
+  height: 48px;
+  width: 48px;
+  padding: 0;
+  margin:  0 2%;
+}
+</style>
---
+++ static/src/views/IocTable.vue
@@ -0,0 +1,238 @@
+<template>
+  <div>
+    <el-table :data="tableData" style="width: 96%" height="100%" id="table">
+      <el-table-column fixed prop="threatInfoID" label="威胁ID" width="120">
+      </el-table-column>
+      <el-table-column prop="ctime" label="时间" :formatter="formateTime">
+      </el-table-column>
+      <el-table-column prop="ioc" label="IP地址"> </el-table-column>
+      <el-table-column prop="domainCount" label="解析域名" width="100">
+      </el-table-column>
+      <el-table-column prop="tagCount" label="标签数量" width="100">
+      </el-table-column>
+      <el-table-column prop="itelCount" label="线索数量" width="100">
+      </el-table-column>
+      <el-table-column prop="tagCount" label="标签数量" width="100">
+      </el-table-column>
+      <el-table-column
+        prop="judge"
+        label="类型"
+        width="100"
+        :formatter="formateJudgeType"
+      >
+      </el-table-column>
+      <el-table-column prop="source" label="数据来源" width="200">
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="totalCount"
+      :page-size="pageSize"
+      id="slider"
+      @current-change="onPageChange"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<style>
+#table {
+  margin: 16px 2%;
+}
+#slider {
+  padding-bottom: 32px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+        {
+          threatInfoID: 14399,
+          ioc: "211.95.50.4",
+          domainCount: "0",
+          tagCount: "5",
+          itelCount: "10",
+          judge: 2,
+          poc: false,
+          ctime: "1657694018004",
+          source: 0,
+        },
+      ],
+      totalCount: 126,
+      pageSize: 10,
+      page: 0,
+      tableData: [],
+    };
+  },
+  mounted: function () {
+    this.getTableData();
+  },
+  methods: {
+    getTableData: function () {
+      var that = this;
+      this.axios
+        .get("https://baidu.com")
+        .then((res) => {
+          console.log(res.data);
+          
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    formateJudgeType: function (row, column, cellValue, index) {
+      var t = cellValue;
+      var result = "——";
+      switch (t) {
+        case 0:
+          result = "安全";
+          break;
+        case 1:
+          result = "未知";
+          break;
+        case 2:
+          result = "恶意";
+          break;
+        default:
+          result = t;
+          break;
+      }
+      return result;
+    },
+    formateTime: function (row, column, cellValue, index) {
+      let time = new Date(parseInt(cellValue)).toLocaleString();
+      return time;
+    },
+    interfaceTrans: function (rawData) {},
+    onPageChange: function () {
+      console.log("pageChange");
+    },
+  },
+};
+</script>
